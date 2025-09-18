f99b5efae053976047f6c73ea1b8f22412b5e175
drezm <151402102+drezm@users.noreply.github.com>
2025-09-18 22:12:58 +0300

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,25 +1,15 @@
-// backend/config/database.js
-
-// **В начале** файла до любых импортов pg
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
 require('dotenv').config();
 const { Pool } = require('pg');
 
-if (!process.env.DB_URL) {
-  console.error('DB_URL is not set');
+if (!process.env.DATABASE_URL) {
+  console.error('DATABASE_URL is not set');
   process.exit(1);
 }
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
-pool.on('error', (err) => console.error('[DB] pool error:', err));
-
-module.exports = {
-  pool,
-  query: (t, p) => pool.query(t, p),
-};
+module.exports = { pool, query: (t,p) => pool.query(t,p) };
 
